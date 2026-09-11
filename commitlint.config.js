d850e6de1888659039bcf7d81dba74e22f7303a9
Mateus Santos de Azevedo <m.santos.azeve@gmail.com>
2026-09-11 01:58:50 -0300

    docs(commitlint): adiciona comentário com os tipos suportados

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,18 @@
+/**
+ * With @commitlint/config-conventional, you will have types like:
+ *
+ * feat      new feature
+ * fix       bug fix
+ * docs      documentation
+ * style     formatting, no code change
+ * refactor  refactoring
+ * test      adding tests
+ * chore     maintenance/configuration
+ * build     changes that affect the build system or external dependencies
+ * ci        CI/CD
+ * perf      performance improvement
+ * revert    reverts a previous commit
+ */
 export default {
   extends: ["@commitlint/config-conventional"],
 };
